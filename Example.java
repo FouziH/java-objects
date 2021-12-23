4789d6ad2eed6ecd2c270c0dc61e95162ef9666f
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-12-23 08:32:58 -0600

    ...

---
+++ Example.java
@@ -0,0 +1,9 @@
+import java.io.Console;
+
+public class Example {
+    public static void main(String[] args) {
+        Console console = System.console();
+        // This is where you write your code....
+    }
+
+}
