f8ba6310d436b1a0bf7507e793b8f31f703c2f3b
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-12-23 18:18:23 -0600

    ...

--- Example.java
+++ Example.java
@@ -7,14 +7,17 @@ public class Example {
         System.out.println("We are making a new PEX Dispenser");
         System.out.printf("Fun Fact: There are %d Pez allowed in every dispenser %n", PezDispenser.MAX_PEZ);
         PezDispenser dispenser = new PezDispenser("Donatello");
+
+        System.out.printf("filling dispenser with delicious PEZ");
+        dispenser.fill();
         // dispenser.characterName ="Darth Vader";
         System.out.printf("This dispenser is %s %n", dispenser.getCharacterName());
 
 
-        GoKart kart = new GoKart("red");
-        // kart.color = "grey";
-        String formatPrint = "This color of the goKart is %s %n";
-        System.out.printf( formatPrint , kart.getGoKart());
+        // GoKart kart = new GoKart("red");
+        // // kart.color = "grey";
+        // String formatPrint = "This color of the goKart is %s %n";
+        // System.out.printf( formatPrint , kart.getGoKart());
     }
 
 }
