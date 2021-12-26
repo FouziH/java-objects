f5ca3f3b2b6752a654a8dfdf28dc29a6441f385f
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-12-26 11:49:52 -0600

    ...

--- PezDispenser.java
+++ PezDispenser.java
@@ -16,7 +16,11 @@ class PezDispenser {
     }
 
     public void fill(int pezAmount){
-        pezCount += pezAmount;
+        int newAmount = pezCount += pezAmount;
+        if(newAmount > MAX_PEZ) {
+           throw new IllegalArgumentException("Too many PEZ") 
+        }
+        pezAmount = newAmount;
     }
     public boolean dispense() {
        boolean wasDispensed = false;
